3bd2143041f3656cf58a8e1d386bf1c7950be09e
darkv314 <60781992+darkv314@users.noreply.github.com>
2020-03-19 16:35:41 -0400

    Delete Clase4.sql

--- Clase4.sql
+++
@@ -1,9 +0,0 @@
-use bookstore;
--- Primer ejercicio Curso 4
-select usuario.usuario, empleado.nombre from usuario inner join empleado on usuario.idempleado=empleado.idempleado; 
--- Segundo ejercicio Curso 4
-select email from empleado where idempleado in(select idempleado from usuario where activo=1);
--- Tercer ejercicio Curso 4
-select count(autor) from publicacion where publicacion.autor = 'Eric G. Coronel Castillo';
--- Cuarto ejercicio Curso 4
-select sum(precio) from venta where idempleado in( select idempleado from empleado where nombre="EMILIO");
